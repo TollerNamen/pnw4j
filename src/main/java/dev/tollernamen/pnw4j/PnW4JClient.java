f73c4ae41af74e89f3d13cc78d1168aa640d0e84
TollerNamen <tollernamen@gmail.com>
2024-05-18 17:30:38 +0200

    lazy push

--- src/main/java/dev/tollernamen/pnw4j/PnW4JClient.java
+++ src/main/java/dev/tollernamen/pnw4j/PnW4JClient.java
@@ -1,4 +1,11 @@
 package dev.tollernamen.pnw4j;
 
 public class PnW4JClient {
+    public static String graphQLEndpoint = "https://api.politicsandwar.com/graphql?api_key=";
+
+    private final String apiKey;
+
+    public PnW4JClient(String apiKey) {
+        this.apiKey = apiKey;
+    }
 }
